4e82761523ff4a6dca932da6944ef4c42f36b05f
Sergey Shlyapugin <shlyapugin@gmail.com>
2019-06-29 23:13:17 +0300

    add travis-ci

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='monobankua',
-    version='0.0.7',
+    version='0.0.8',
     author='Sergey Shlyapugin',
     author_email='shlyapugin@gmail.com',
     description='Monobank.ua API client',
