a342b389f0832d6939abccbb8c6407a1a48c6751
Sergey Shlyapugin <shlyapugin@gmail.com>
2019-06-29 18:25:41 +0300

    add license to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='monobankua',
-    version='0.0.4',
+    version='0.0.6',
     author='Sergey Shlyapugin',
     author_email='shlyapugin@gmail.com',
     description='Monobank.ua API client',
@@ -24,4 +24,5 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
+    license='GPL version 3'
 )
