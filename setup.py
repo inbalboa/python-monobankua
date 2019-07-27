8a7fd99bf34e5a64d0c5bbd3d976b2e640264607
inbalboa <shlyapugin@gmail.com>
2019-07-28 00:29:46 +0300

    fixed errors

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_requires():
 
 setuptools.setup(
     name='monobankua',
-    version='0.2.3',
+    version='0.2.4',
     author='Sergey Shlyapugin',
     author_email='shlyapugin@gmail.com',
     description='Monobank.ua API client',
