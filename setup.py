111caf9971d53e70124eee9f155519ed4b48e8f6
Sergey Shlyapugin <shlyapugin@gmail.com>
2019-08-19 20:47:26 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_requires():
 
 setuptools.setup(
     name='monobankua',
-    version='0.2.7',
+    version='0.2.8',
     author='Sergey Shlyapugin',
     author_email='shlyapugin@gmail.com',
     description='Monobank.ua API client',
